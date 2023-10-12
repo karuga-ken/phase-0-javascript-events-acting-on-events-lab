bbcf6f82e2c5c0b87c9a9467e5823aae292337ac
Karuga Mbugua <karugambugua@icloud.com>
2023-10-12 23:03:15 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+const moveDodger = document.getElementById('dodger')
+
+function moveDodgerLeft (){
+    const leftNum = moveDodger.style.left.replace("px", "");
+    const left = parseInt(leftNum, 10);
+
+    if(left > 0 ){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft()
+    }
+})
+
+function moveDodgerRight(){
+    const leftNum = moveDodger.style.left.replace("px", "");
+    const left = parseInt(leftNum, 10);
+
+    if(left < 180){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e){
+    if(e.key === "ArrowRight"){
+        moveDodgerRight()
+    }
+})
